25787e92a65b46c52920720175cdfa7c3386ffb0
Ilnur Nasybullin <ilnur.nasybullin2000@yandex.ru>
2023-01-04 19:51:27 +0300

    fix bug with finding alternative solution

--- mip/src/main/java/io/github/ilnurnasybullin/math/mip/MipSolver.java
+++ mip/src/main/java/io/github/ilnurnasybullin/math/mip/MipSolver.java
@@ -458,6 +458,7 @@ public class MipSolver {
             }
 
             List<Simplex> alternativeSolutions = simplex.findAlternativeSolutions(executor);
+            alternativeSolutions.add(simplex);
 
             var tasks = alternativeSolutions.stream()
                     .map(alternativeSolution -> (Runnable) () -> compareAndResolve(alternativeSolution, new Integer[xCount * 2], -1))
